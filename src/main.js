79fc3bc4c8ce04587552539727bdc926c4356dfb
JohnReyGimenez <gimenezjr5@gmail.com>
2025-07-19 17:08:08 +0800

    added hero.css and styling to hero componenents

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import 'modern-normalize/modern-normalize.css';
 import '../styles/style.css';
 import '../styles/components/header.css';
+import '../styles/components/hero.css';
 import '../styles/utils.css';
