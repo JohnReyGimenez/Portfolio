d54d3ae1d67b52d9316653a8018a18d0157f7cb3
JohnReyGimenez <gimenezjr5@gmail.com>
2025-07-20 20:30:20 +0800

    added: basic css to about components

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import 'modern-normalize/modern-normalize.css';
 import '../styles/style.css';
 import '../styles/components/header.css';
 import '../styles/components/hero.css';
+import '../styles/components/about.css'
 import '../styles/utils.css';
